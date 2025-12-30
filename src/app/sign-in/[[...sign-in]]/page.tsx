7a84c6ece46a4c0decb5d4a8f4f0971cebe8f06c
z-scd <sagardey9876@gmail.com>
2025-12-31 03:55:59 +0600

    Remove some unnecessary files

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <SignIn />
-    </div>
-  );
-}
--- src/services/clerk/components/ClerkProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { ClerkProvider as OriginalClerkProvider } from "@clerk/nextjs";
-
-export default function ClerkProvider({ children }: { children: ReactNode }) {
-  return <OriginalClerkProvider>{children}</OriginalClerkProvider>;
-}
